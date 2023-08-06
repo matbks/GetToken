41a53deeee19a1137a8a8631e8ce08119bd81b59
Backhaus <44726485+matbks@users.noreply.github.com>
2023-08-06 03:55:09 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = 3000;
+
+// Middleware para analisar o corpo das solicitações POST
+app.use(bodyParser.json());
+
+app.post('/authenticate', (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  // Verifique se o nome de usuário e a senha correspondem aos valores esperados
+  if (username === 'contato@moovi.education' && password === '123456') {
+    // Se os valores forem corretos, retorne o token
+    res.json({ token: 'adwidjWA124jllkkkk' });
+  } else {
+    // Se os valores estiverem incorretos, retorne um erro
+    res.status(401).json({ error: 'Authentication failed' });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
