e9359844529ffea459713fe62d4d0ac2ca8d1344
Backhaus <44726485+matbks@users.noreply.github.com>
2023-08-06 03:58:47 -0300

    ss

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
-const port = 3002;
+const port = 3009;
 
 // Middleware para analisar o corpo das solicitações POST
 app.use(bodyParser.json());
